b1e77752c937a6ee8c5ac1989ce1651039dd2ebe
ClementBECRET <clementbecret@aol.com>
2017-05-12 11:21:12 +0200

    creation de la branche avec bot perso

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  token: 'MzEyMzM0OTg0MDk2Nzc2MjAz.C_ZkNg.JJdD1ne8avZDl7w9PVaM85igqVw',
+  token: 'MzEyNTA5NTcxNzk5MzE4NTI5.C_cGvA.5ft2FNYBFQ46ZiMCMD8NjOLnL2w',
   channel: 'channelid'
 }
