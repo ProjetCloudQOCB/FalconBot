84adc5f0c76d46d58356a6eec63bb47340f90311
Antoine Quinquenel <aquinq@hotmail.fr>
2017-05-12 11:05:34 +0200

    Ajout du token du bot FalconBot

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  token: 'MzEyMzM0OTg0MDk2Nzc2MjAz.C_ZkNg.JJdD1ne8avZDl7w9PVaM85igqVw',
+  token: 'MzEyNTA2NzE2MjMzMjY5MjQ4.C_cKQg.zXrPFog_6rIqjrIKoVPZSE8l3i4',
   channel: 'channelid'
 }
