618092a620045618435476375794fd36b6baa8c4
ClementBECRET <clementbecret@aol.com>
2017-06-12 18:09:53 +0200

    ajout de twitter

--- config.js
+++ config.js
@@ -1,4 +1,10 @@
 module.exports = {
   token: 'MzEyNTA5NTcxNzk5MzE4NTI5.C_cGvA.5ft2FNYBFQ46ZiMCMD8NjOLnL2w',
-  channel: '307410406211584001'
+  channel: '307410406211584001',
+  twitter: {
+    consumer_key: 'srrgmvkfNyqWah0WduJVN9So2',
+    consumer_secret: 'BaOnPRz0RxQMDp2fo3FRrooYuighCUHCvMALaGjH7P8cI03Vp5',
+    access_token: '865508843748335616-jZwkcTdsFDSKI4hJbbO8S4IdTKn7U5n',
+    access_token_secret: 'Mb1AIanJ4RtxYWsYYIQqg8RNMVkEzH9VO2WugOekVJT2K'
+  }
 }
